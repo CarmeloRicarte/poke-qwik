a6846854343608d7ba3c52bb75711c52c8ac048a
Carmelo Ricarte Rocamora <carmeloricarterocamora@gmail.com>
2023-06-22 10:19:43 +0200

    style: pass prettier to project

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -2,19 +2,19 @@ import { component$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
 
 export default component$(() => {
-  return (
-    <>
-      <span class='text-5xl'>Hola</span>
-    </>
-  );
+    return (
+        <>
+            <span class="text-5xl">Hola</span>
+        </>
+    );
 });
 
 export const head: DocumentHead = {
-  title: 'Welcome to Qwik',
-  meta: [
-    {
-      name: 'description',
-      content: 'Qwik site description',
-    },
-  ],
+    title: 'Welcome to Qwik',
+    meta: [
+        {
+            name: 'description',
+            content: 'Qwik site description',
+        },
+    ],
 };
