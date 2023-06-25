c540376976d1127e27151f8df50d194e0103f828
Carmelo Ricarte Rocamora <carmeloricarterocamora@gmail.com>
2023-06-25 11:13:55 +0200

    feat: create new routes

---
+++ src/routes/pokemons/list-client/index.tsx
@@ -0,0 +1,9 @@
+import { component$ } from '@builder.io/qwik';
+
+export default component$(() => {
+    return (
+        <div>
+            <h1>Hello World - List Client</h1>
+        </div>
+    );
+});
---
+++ src/routes/pokemons/list-ssr/index.tsx
@@ -0,0 +1,9 @@
+import { component$ } from '@builder.io/qwik';
+
+export default component$(() => {
+    return (
+        <div>
+            <h1>Hello World</h1>
+        </div>
+    );
+});
