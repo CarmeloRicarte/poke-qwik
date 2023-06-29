ea39e08d7bde1945cb072ffbf14dc3694677024f
Carmelo Ricarte Rocamora <carmeloricarterocamora@gmail.com>
2023-06-29 20:44:08 +0200

    feat: add link to index for qwik logo

--- src/components/shared/navbar/navbar.tsx
+++ src/components/shared/navbar/navbar.tsx
@@ -8,9 +8,9 @@ export default component$(() => {
         <header class={styles.header}>
             <div class={['container', styles.wrapper]}>
                 <div class={styles.logo}>
-                    <a href="/" title="qwik">
+                    <Link href="/" title="qwik">
                         <QwikLogo height={50} />
-                    </a>
+                    </Link>
                 </div>
                 <ul>
                     <li>
