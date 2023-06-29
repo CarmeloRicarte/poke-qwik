1964f5392d4faaf2914a105f2fc64af549f44e4b
Carmelo Ricarte Rocamora <carmeloricarterocamora@gmail.com>
2023-06-29 20:58:06 +0200

    feat: navigate to pokemon detail page on click image

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,23 +1,27 @@
 import { $, component$, useSignal } from '@builder.io/qwik';
-import type { DocumentHead } from '@builder.io/qwik-city';
+import { useNavigate, type DocumentHead } from '@builder.io/qwik-city';
 import { PokemonImage } from '../components/pokemons/pokemon-image';
 
 export default component$(() => {
     const pokemonId = useSignal(1); // use it for booleans, strings, numbers
     const showBackImage = useSignal(false);
     const isPokemonVisible = useSignal(true);
-
+    const nav = useNavigate();
     const changePokemonId = $((value: number) => {
         if (pokemonId.value + value <= 0) return;
         pokemonId.value += value;
     });
+
+    const goToPokemon = $(async () => await nav(`/pokemon/${pokemonId.value}`));
 
     return (
         <>
             <span class="text-2xl">Buscador simple</span>
             <span class="text-9xl">{pokemonId}</span>
 
-            <PokemonImage id={pokemonId.value} backImage={showBackImage.value} isVisible={isPokemonVisible.value} />
+            <div class="cursor-pointer" onClick$={async () => goToPokemon()}>
+                <PokemonImage id={pokemonId.value} backImage={showBackImage.value} isVisible={isPokemonVisible.value} />
+            </div>
 
             <div class="mt-2">
                 <button
