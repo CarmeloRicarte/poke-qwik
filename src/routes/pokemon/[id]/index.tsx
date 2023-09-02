403d199f3901cca6ea910d818fe79f8ab2b81c9d
Carmelo Ricarte Rocamora <carmeloricarterocamora@gmail.com>
2023-09-02 19:02:41 +0200

    fix(pokemon-image): load core

--- src/routes/pokemon/[id]/index.tsx
+++ src/routes/pokemon/[id]/index.tsx
@@ -1,7 +1,21 @@
 import { component$ } from '@builder.io/qwik';
-import { useLocation } from '@builder.io/qwik-city';
+import { routeLoader$ } from '@builder.io/qwik-city';
+import { PokemonImage } from '../../../components/pokemons/pokemon-image';
+
+export const usePokemonId = routeLoader$<number>(({ params, redirect }) => {
+    const id = Number(params.id);
+    if (isNaN(id) || id <= 0 || id > 1000) {
+        throw redirect(301, '/');
+    }
+    return id;
+});
 
 export default component$(() => {
-    const { params } = useLocation();
-    return <span class="text-5xl">Pokemon: {params.id}</span>;
+    const pokemonId = usePokemonId();
+    return (
+        <>
+            <span class="text-5xl">Pokemon: {pokemonId.value}</span>
+            <PokemonImage id={pokemonId.value} />
+        </>
+    );
 });
