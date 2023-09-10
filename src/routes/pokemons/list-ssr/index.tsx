f1661fb74ca224de64089c68ea3001362f53652d
Carmelo Ricarte Rocamora <carmeloricarterocamora@gmail.com>
2023-09-10 17:08:37 +0200

    feat(list-ssr): add logic to pagination buttons

--- src/routes/pokemons/list-ssr/index.tsx
+++ src/routes/pokemons/list-ssr/index.tsx
@@ -1,26 +1,40 @@
-import { component$ } from '@builder.io/qwik';
-import { Link, routeLoader$, type DocumentHead } from '@builder.io/qwik-city';
+import { component$, useComputed$ } from '@builder.io/qwik';
+import { Link, routeLoader$, useLocation, type DocumentHead } from '@builder.io/qwik-city';
 import type { PokemonListResponseT } from './pokemonListResponseT';
 
-export const usePokemonList = routeLoader$(async () => {
-    const resp = await fetch('https://pokeapi.co/api/v2/pokemon?limit=10&offset=0');
+export const usePokemonList = routeLoader$(async ({ query, redirect, pathname }) => {
+    const offset = Number(query.get('offset') || '0');
+    if (offset < 0 || isNaN(offset)) {
+        throw redirect(301, pathname);
+    }
+    const resp = await fetch(`https://pokeapi.co/api/v2/pokemon?limit=10&offset=${offset}`);
     const data = (await resp.json()) as PokemonListResponseT;
     return data.results;
 });
 
 export default component$(() => {
     const pokemons = usePokemonList();
+    const location = useLocation();
+
+    const currentOffset = useComputed$(() => {
+        const offsetString = new URLSearchParams(location.url.search);
+        return Number(offsetString.get('offset')) || 0;
+    });
     return (
         <>
             <div class="flex flex-col">
                 <span class="my-5 text-5xl">Estado</span>
-                <span>Página actual: XXXX</span>
-                <span>Está cargando página: xxxx</span>
+                <span>Offset: {currentOffset}</span>
+                <span>Está cargando página: {location.isNavigating ? 'Sí' : 'No'}</span>
             </div>
 
             <div class="mt-10">
-                <Link class="btn btn-primary mr-2">Anteriores</Link>
-                <Link class="btn btn-primary mr-2">Siguientes</Link>
+                <Link href={`/pokemons/list-ssr/?offset=${currentOffset.value - 10}`} class="btn btn-primary mr-2">
+                    Anteriores
+                </Link>
+                <Link href={`/pokemons/list-ssr/?offset=${currentOffset.value + 10}`} class="btn btn-primary mr-2">
+                    Siguientes
+                </Link>
             </div>
 
             <div class="grid grid-cols-6 mt-5">
