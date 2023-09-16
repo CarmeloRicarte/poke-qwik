0cc5797e5fcbfb74b377bd15df2fbde3cfbb61d9
Carmelo Ricarte Rocamora <carmeloricarterocamora@gmail.com>
2023-09-16 09:53:41 +0200

    feat(ListClient): implement infinite scroll for showing pokemons

--- src/routes/pokemons/list-client/index.tsx
+++ src/routes/pokemons/list-client/index.tsx
@@ -1,4 +1,4 @@
-import { component$, useStore, useTask$ } from '@builder.io/qwik';
+import { $, component$, useOnDocument, useStore, useTask$ } from '@builder.io/qwik';
 import type { DocumentHead } from '@builder.io/qwik-city';
 import { PokemonImage } from '../../../components/pokemons/pokemon-image';
 import { getSmallPokemons } from '../helpers';
@@ -8,30 +8,47 @@ export default component$(() => {
     const pokemonState = useStore<PokemonPageStateT>({
         currentPage: 0,
         pokemons: [],
+        isLoading: false,
+        isFinalPage: false,
     });
+
+    const GRACE_MARGIN_CURRENT_SCROLL = 200;
+    const POKEMONS_PER_PAGE = 10;
 
     useTask$(async ({ track }) => {
         track(() => pokemonState.currentPage);
-
-        const pokemons = await getSmallPokemons(pokemonState.currentPage * 10);
+        const pokemons = await getSmallPokemons(pokemonState.currentPage * POKEMONS_PER_PAGE);
         pokemonState.pokemons = [...pokemonState.pokemons, ...pokemons];
+        pokemonState.isLoading = false;
+        pokemonState.isFinalPage = pokemons.length === 0;
     });
+
+    useOnDocument(
+        'scroll',
+        $(() => {
+            const maxScroll = document.body.scrollHeight;
+            const currentScroll = window.scrollY + window.innerHeight;
+
+            if (
+                currentScroll + GRACE_MARGIN_CURRENT_SCROLL >= maxScroll &&
+                !pokemonState.isLoading &&
+                !pokemonState.isFinalPage
+            ) {
+                pokemonState.isLoading = true;
+                pokemonState.currentPage++;
+            }
+        }),
+    );
 
     return (
         <>
             <div class="flex flex-col">
                 <span class="my-5 text-5xl">Estado</span>
                 <span>P;agina actual: {pokemonState.currentPage} </span>
-                <span>Está cargando: </span>
+                <span>Está cargando: {pokemonState.isLoading}</span>
             </div>
 
-            <div class="mt-10">
-                <button type="button" onClick$={() => pokemonState.currentPage++} class="btn btn-primary mr-2">
-                    Siguientes
-                </button>
-            </div>
-
-            <div class="grid grid-cols-6 mt-5">
+            <div class="grid sm:grid-cols-2 md:grid-cols-5 xl:grid-cols-7 mt-5">
                 {pokemonState.pokemons?.map(({ name, id }) => (
                     <div key={name} class="m-5 flex flex-col justify-center items-center">
                         <PokemonImage id={id} />
--- src/routes/pokemons/list-client/pokemonListClientT.ts
+++ src/routes/pokemons/list-client/pokemonListClientT.ts
@@ -3,4 +3,6 @@ import type { SmallPokemonT } from '../types';
 export type PokemonPageStateT = {
     currentPage: number;
     pokemons: SmallPokemonT[];
+    isLoading: boolean;
+    isFinalPage: boolean;
 };
