74541a26fa3db058b1c124b0647bedfbf5df70d2
Carmelo Ricarte Rocamora <carmeloricarterocamora@gmail.com>
2023-09-17 18:42:53 +0200

    feat(pokemon-provider): save and read data of pokemon in localStorage

--- src/context/pokemon/pokemon-provider.tsx
+++ src/context/pokemon/pokemon-provider.tsx
@@ -1,8 +1,9 @@
-import { Slot, component$, useContextProvider, useStore } from '@builder.io/qwik';
+import { Slot, component$, useContextProvider, useStore, useVisibleTask$ } from '@builder.io/qwik';
 import { PokemonGameContext, type PokemonGameStateT } from './pokemon-game.context';
 import { PokemonListContext, type PokemonListStateT } from './pokemon-list.context';
 
 export const PokemonProvider = component$(() => {
+    const POKEMON_GAME_KEY = 'pokemon-game';
     const pokemonGameStore = useStore<PokemonGameStateT>({
         pokemonId: 1,
         isBackImageShowed: false,
@@ -18,5 +19,28 @@ export const PokemonProvider = component$(() => {
 
     useContextProvider(PokemonGameContext, pokemonGameStore);
     useContextProvider(PokemonListContext, pokemonListStore);
+
+    useVisibleTask$(() => {
+        if (localStorage.getItem(POKEMON_GAME_KEY)) {
+            const {
+                pokemonId = 10,
+                isBackImageShowed = false,
+                isPokemonVisible = true,
+            } = JSON.parse(localStorage.getItem(POKEMON_GAME_KEY)!) as PokemonGameStateT;
+            pokemonGameStore.pokemonId = pokemonId;
+            pokemonGameStore.isBackImageShowed = isBackImageShowed;
+            pokemonGameStore.isPokemonVisible = isPokemonVisible;
+        }
+    });
+
+    useVisibleTask$(({ track }) => {
+        track(() => [
+            pokemonGameStore.pokemonId,
+            pokemonGameStore.isBackImageShowed,
+            pokemonGameStore.isPokemonVisible,
+        ]);
+        localStorage.setItem(POKEMON_GAME_KEY, JSON.stringify(pokemonGameStore));
+    });
+
     return <Slot />;
 });
