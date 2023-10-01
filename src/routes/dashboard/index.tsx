84a5eafa1689a50549b3284c4111b4b18821679b
Carmelo Ricarte Rocamora <carmeloricarterocamora@gmail.com>
2023-10-01 09:26:35 +0200

    feat: create routes and layout for dashboard, login and add links to navbar

---
+++ src/routes/dashboard/index.tsx
@@ -0,0 +1,10 @@
+import { component$ } from '@builder.io/qwik';
+
+export default component$(() => {
+    return (
+        <>
+            <span>Admin Dashboard</span>
+            <p>Esta ruta debe ser privada</p>
+        </>
+    );
+});
